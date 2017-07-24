a3bb583deb0ffa91d715128b9753dd3eaac71a7c
Arkaniy <lexaalek@mail.ru>
2017-07-24 22:41:44 +0300

    lesson 3

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+posts = Post.create([
+	{
+		title: 'Gentrify flannel',
+		body: 'man bun cred beard marfa. Af keytar woke VHS. Deep v pok pok pour-over poke post-ironic hashtag chambray disrupt air plant direct trade freegan unicorn. Godard ennui chartreuse +1 drinking vinegar chambray pork belly. Lomo synth fashion axe mlkshk hashtag la croix tumblr fanny pack pitchfork tilde. Tofu roof party messenger bag, cronut dreamcatcher pitchfork pinterest gentrify portland shaman yr meh meggings kickstarter tousled.',
+		description: 'Air plant copper mug DIY man bun iPhone, scenester food truck actually roof party vegan fixie hot chicken. Pop-up actually chillwave, before they sold out whatever pok pok copper mug YOLO hashtag XOXO plaid swag pitchfork cronut. Bushwick try-hard beard, fingerstache hot chicken franzen twee.'
+	},
+	{
+		title: 'Meh stumptown normcore',
+		body: 'Meh stumptown normcore, godard salvia cardigan austin offal small batch slow-carb hella pinterest master cleanse air plant. Brooklyn tacos cardigan occupy organic subway tile umami. Put a bird on it four loko art party marfa banh mi air plant trust fund, mustache prism butcher hoodie biodiesel shabby chic intelligentsia',
+		description: 'DIY next level chicharrones activated charcoal YOLO hot chicken roof party whatever bicycle rights wayfarers offal cloud bread stumptown. Meggings mumblecore photo booth, enamel pin farm-to-table literally schlitz edison bulb. Ramps literally microdosing, farm-to-table venmo squid man braid sustainable enamel pin cornhole. Fanny pack tbh authentic jianbing VHS quinoa. Try-hard palo santo viral fashion axe fam hot chicken. Photo booth cray lyft tumblr, locavore hell of gastropub salvia. Intelligentsia snackwave prism wolf church-key, iPhone authentic bushwick ennui hoodie. Godard copper mug retro post-ironic health goth.'
+	},
+	{
+		title: 'Gentrify flannel 2',
+		body: 'man bun cred beard marfa. Af keytar woke VHS. Deep v pok pok pour-over poke post-ironic hashtag chambray disrupt air plant direct trade freegan unicorn. Godard ennui chartreuse +1 drinking vinegar chambray pork belly. Lomo synth fashion axe mlkshk hashtag la croix tumblr fanny pack pitchfork tilde. Tofu roof party messenger bag, cronut dreamcatcher pitchfork pinterest gentrify portland shaman yr meh meggings kickstarter tousled.',
+		description: 'Air plant copper mug DIY man bun iPhone, scenester food truck actually roof party vegan fixie hot chicken. Pop-up actually chillwave, before they sold out whatever pok pok copper mug YOLO hashtag XOXO plaid swag pitchfork cronut. Bushwick try-hard beard, fingerstache hot chicken franzen twee.'
+	},
+	{
+		title: 'Meh stumptown normcore 2',
+		body: 'Meh stumptown normcore, godard salvia cardigan austin offal small batch slow-carb hella pinterest master cleanse air plant. Brooklyn tacos cardigan occupy organic subway tile umami. Put a bird on it four loko art party marfa banh mi air plant trust fund, mustache prism butcher hoodie biodiesel shabby chic intelligentsia',
+		description: 'DIY next level chicharrones activated charcoal YOLO hot chicken roof party whatever bicycle rights wayfarers offal cloud bread stumptown. Meggings mumblecore photo booth, enamel pin farm-to-table literally schlitz edison bulb. Ramps literally microdosing, farm-to-table venmo squid man braid sustainable enamel pin cornhole. Fanny pack tbh authentic jianbing VHS quinoa. Try-hard palo santo viral fashion axe fam hot chicken. Photo booth cray lyft tumblr, locavore hell of gastropub salvia. Intelligentsia snackwave prism wolf church-key, iPhone authentic bushwick ennui hoodie. Godard copper mug retro post-ironic health goth.'
+	}
+])

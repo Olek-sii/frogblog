2de1b115bebcdf7c38b24dd3658a4251c9726191
Arkaniy <lexaalek@mail.ru>
2017-07-26 21:22:45 +0300

    add simplecov

--- spec/features/blog_posts_controller_spec.rb
+++ spec/features/blog_posts_controller_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
-describe "Blog posts titles" do
+describe "Blog posts pages" do
   subject { page }
 
-  describe "root path" do
+  describe "about page" do
+    before { visit about_path }
+    it { should have_title('About | FrogBlog') }
+  end
+
+  describe "contact page" do
+    before { visit contact_path }
+    it { should have_title('Contact | FrogBlog') }
+  end
+
+  describe "root page" do
     before { visit root_path }
     it { should have_title('Posts | FrogBlog') }
+  end
+
+  describe "my blog page" do
+    before { visit author_posts_path }
+    it { should have_title('Posts | FrogBlog') }
+  end
+
+  describe "new page" do
+    before { visit new_author_post_path }
+    it { should have_title('New post | FrogBlog') }
   end
 end
